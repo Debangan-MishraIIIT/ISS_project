02be6ae393afa0becd5cd20b3c87fb628d31faef
ShahAkshat0608 <127509211+ShahAkshat0608@users.noreply.github.com>
2023-05-08 17:20:28 +0530

    css + some backend

--- TuneTrance/p.py
+++ TuneTrance/p.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect,jsonify,request
 import sqlite3
 
 app = Flask(__name__)
@@ -40,6 +40,28 @@ def delete_item(item_id):
     conn.commit()
     conn.close()
     return redirect("/")
+
+@app.route('/add_to_playlist', methods=['POST'])
+def add_to_playlist():
+    song_id = request.form.get('song_id')
+
+    # Check if the song is already in the playlist
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM playlist WHERE SongID=?", (song_id,))
+    result = cursor.fetchone()
+    if result[0] > 0:
+        cursor.close()
+        conn.close()
+        return jsonify({'success': False})
+
+    # Insert the song into the playlist
+    cursor.execute("INSERT INTO playlist (SongId) VALUES (?)", (song_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({'success': True})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
